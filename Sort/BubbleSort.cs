61d83368f4fbe9de0b3b3ee891e05a79aba3ee1f
Meenakshi <dgrmeenakshi@gmail.com>
2019-01-11 23:33:07 +0530

    Bubble Sort

---
+++ Sort/BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemSolving.Problems;
+
+namespace ProblemSolving.Sort
+{
+   public class BubbleSort
+    {
+        public void Sort(int[] Input)
+        {
+            int size = Input.Length;
+            for (int k = 0; k < size - 1; k++)
+            {
+                for (int i = 0; i < size - 2; i++)
+                {
+                    if (Input[i] > Input[i + 1])
+                    {
+                        Swap(Input, i, i+1);
+                    }
+                    
+                }
+            }
+     
+        }
+
+        private static void Swap(int[] array, int index, int nextElementIndex)
+        {
+            array[index] = array[index] + array[nextElementIndex];
+            array[nextElementIndex] = array[index] - array[nextElementIndex];
+            array[index] = array[index] - array[nextElementIndex];
+        }
+    }
+}
