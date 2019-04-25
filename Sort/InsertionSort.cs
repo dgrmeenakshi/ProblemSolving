09ff82795268777c190b6ade34b8566dd16ade3c
Meenakshi <dgrmeenakshi@gmail.com>
2019-04-25 22:30:42 +0530

    Insertion Sort

--- Sort/InsertionSort.cs
+++ Sort/InsertionSort.cs
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-﻿using System;
+
+using System;
 
 namespace ProblemSolving.Sort
 {
@@ -30,47 +30,8 @@ namespace ProblemSolving.Sort
             array[index] = array[index] - array[nextElementIndex];
 
         }
-
-
-
-
     }
-=======
-﻿using System;
-
-namespace ProblemSolving.Sort
-{
-    public class InsertionSort
-    {
-        public static int[] Sort(int[] array)
-        {
-            int size = array.Length;
-            for (int index = 0; index < size; index++)
-            {
-                for (int nextElementIndex = index + 1; nextElementIndex < size; nextElementIndex++)
-                {
-                    if (array[index] > array[nextElementIndex])
-                    {
-                        Swap(array, index, nextElementIndex);
-                    }
-                }
-
-            }
-
-            return array;
-        }
-
-        private static void Swap(int[] array, int index, int nextElementIndex)
-        {
-            array[index] = array[index] + array[nextElementIndex];
-            array[nextElementIndex] = array[index] - array[nextElementIndex];
-            array[index] = array[index] - array[nextElementIndex];
-
-        }
-
-
-
-
-    }
->>>>>>> f70af92a9c75fada945047267cb93eb9cc4f616f
 }
+
+
+
