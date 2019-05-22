546438ff25b6cb14acaa0c7617f15537cfe0f216
Meenakshi <dgrmeenakshi@gmail.com>
2019-05-22 22:22:22 +0530

    Triangle Pattern

---
+++ Problems/PrintTrianglePattern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Problems
+{
+   public class PrintTrianglePattern
+    {
+        public void TrianglePattern(int input)
+        {
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = input; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write(" * ");
+                }
+
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
