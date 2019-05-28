707c94b69deaf9162ec3612dd6820a54cbb177b3
Meenakshi <dgrmeenakshi@gmail.com>
2019-05-28 11:14:12 +0530

    Singleton Design Pattern

---
+++ DesignPattern/Singleton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ProblemSolving.DesignPattern
+{
+    public class Singleton
+    {
+        private static int _counter;
+        private static readonly object Obj = new object();
+        /*
+        * Private constructor ensures that object is not
+        * instantiated other than with in the class itself
+        */
+        private Singleton()
+        {
+            _counter++;
+            Console.WriteLine("Only " + _counter + " Instance is created");
+        }
+        private static Singleton _instance;
+        /*
+         * public property is used to return only one instance of the class
+         * leveraging on the private property
+         * Made it static so it could be used because Singleton class object creation not possible using new Operator
+         */
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Obj)// it's a keyword which ensures one thread is executing a piece of code at one time. 
+                    {
+                        if (_instance == null)
+                            _instance = new Singleton();
+                    }
+                }
+                return _instance;
+            }
+        }
+        /*
+         * Public method which can be invoked through the singleton instance
+         */
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
+
---
+++ Problems/Matrix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Problems
+{
+    public class Matrix
+    {
+        public int[,] EnterMatrixInts(int row, int col, int[,] arrInts)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrInts[i,j]= int.Parse(Console.ReadLine()+ "  ");
+                 
+                }
+
+                Console.WriteLine(" ");
+            }
+
+            return arrInts;
+        }
+
+        public void PrintMatrixDiagonaly(int row , int col, int[,] arrInts)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.WriteLine(arrInts[i,j] + " ");
+                    }
+                }
+            }
+        }
+
+        public void PrintMatrixInOneLine()
+        {
+
+        }
+
+        public void PrintSpiralMatrix()
+        {
+
+        }
+    }
+}
