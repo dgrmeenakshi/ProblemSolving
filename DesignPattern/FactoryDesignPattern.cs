2835f34618945ed3ead5c2849df4cc2bbebaf9a5
Meenakshi <dgrmeenakshi@gmail.com>
2019-06-04 21:57:44 +0530

    FactoryDesignPattern

---
+++ DesignPattern/FactoryDesignPattern.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ProblemSolving.DesignPattern
+{
+    //Related to object creation without exposing the creation logic to the client and uses the common interface.
+
+    public class FactoryDesignPattern
+    {
+        public enum OMS
+        {
+            connexx,
+            noah
+        }
+        public IApplication GetApplicationFactory(OMS oms)
+        {
+            switch (oms)
+            {
+                case OMS.connexx:
+                    return new Connexx();
+                case OMS.noah:
+                    return new Noah();
+            }
+
+            return null;
+        }
+    }
+
+    public interface IApplication
+    {
+        void Start();
+        void Stop();
+    }
+
+    public class Connexx : IApplication
+    {
+        public void Start()
+        {
+            Console.WriteLine("Connexx Launched");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Connexx Closed");
+        }
+    }
+
+    public class Noah : IApplication
+    {
+        public void Start()
+        {
+            Console.WriteLine("Noah Launhed");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Noah closed");
+        }
+    }
+
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            FactoryDesignPattern factory = new FactoryDesignPattern();
+            var appliaction = factory.GetApplicationFactory(FactoryDesignPattern.OMS.noah);
+            appliaction.Start();
+            Console.ReadLine();
+        }
+
+
+    }
+}
