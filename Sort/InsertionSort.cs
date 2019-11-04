3a921737bd0180c9fca20db9504f622ff36f5acf
Meenakshi Rana <34996930+dgrmeenakshi@users.noreply.github.com>
2019-11-04 14:49:59 +0530

    UpdatedInsertionSort

--- Sort/InsertionSort.cs
+++ Sort/InsertionSort.cs
@@ -1,26 +1,22 @@
-
 using System;
 
 namespace ProblemSolving.Sort
 {
+    /// <summary>
+    ///     Insertion Sort builds the final sorted array one item at a time.
+    ///     Select the first element and compare with all the remaining elements in array, Swap if order is incorrect.
+    /// </summary>
     public class InsertionSort
     {
-        public static int[] Sort(int[] array)
+        public static void Sort(int[] array)
         {
-            int size = array.Length;
-            for (int index = 0; index < size; index++)
-            {
-                for (int nextElementIndex = index + 1; nextElementIndex < size; nextElementIndex++)
-                {
-                    if (array[index] > array[nextElementIndex])
-                    {
-                        Swap(array, index, nextElementIndex);
-                    }
-                }
+            var size = array.Length;
+            for (var index = 0; index < size; index++)
+            for (var nextElementIndex = index + 1; nextElementIndex < size; nextElementIndex++)
+                if (array[index] > array[nextElementIndex])
+                    Swap(array, index, nextElementIndex);
 
-            }
-
-            return array;
+            PrintArray(array);
         }
 
         private static void Swap(int[] array, int index, int nextElementIndex)
@@ -28,10 +24,11 @@ namespace ProblemSolving.Sort
             array[index] = array[index] + array[nextElementIndex];
             array[nextElementIndex] = array[index] - array[nextElementIndex];
             array[index] = array[index] - array[nextElementIndex];
+        }
 
+        private static void PrintArray(int[] arr)
+        {
+            for (var i = 0; i < arr.Length; i++) Console.Write(arr[i]);
         }
     }
 }
-
-
-
