adaa4e612cf4dfbdd13f2e6ced92e4f98a37ffcc
Meenakshi Rana <34996930+dgrmeenakshi@users.noreply.github.com>
2019-11-07 15:46:32 +0530

    First non repeated char in word

---
+++ Problems/FirstNonRepeatedCharacterinWord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProblemSolving.Problems
+{
+    public class FirstNonRepeatedCharacterinWord
+    {
+        public static void FirstNonRepeatedCharinWord(string s)
+        {
+            var dc = new Dictionary<string, int>();
+            foreach (var cr in s)
+                if (dc.ContainsKey(cr.ToString()))
+                    dc[cr.ToString()]++;
+                else
+                    dc.Add(cr.ToString(), 1);
+
+            foreach (var kvp in dc)
+            {
+                if (kvp.Value == 1)
+                {
+                    Console.WriteLine($"First non repeated is {kvp.Key}");
+                    break;
+                }
+
+                Console.WriteLine("All characters are repeating");
+            }
+        }
+    }
+}
