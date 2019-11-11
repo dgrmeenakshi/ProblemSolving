d43886c8a4c348b2a50aa0abf4d4acd968f58022
Meenakshi Rana <34996930+dgrmeenakshi@users.noreply.github.com>
2019-11-11 15:34:34 +0530

    MergeTwoStrings

---
+++ Problems/MergeTwoStrings.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace ProblemSolving.Problems
+{
+    public class MergeTwoStrings
+    {
+        // Merge two string of different lengths s1- abc, s2- de then output= adbec
+        public string MergeStrings(string s1, string s2)
+        {
+            var maxLength = s1.Length > s2.Length ? s1.Length : s2.Length;
+            var stringBuilder = new StringBuilder();
+            for (var i = 0; i < maxLength; i++)
+            {
+                if (i < s1.Length) stringBuilder.Append(s1[i]);
+
+                if (i < s2.Length) stringBuilder.Append(s2[i]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
