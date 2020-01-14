c93df78247df26b552013bf64be3b3d759147001
Meenakshi <dgrmeenakshi@gmail.com>
2020-01-14 15:33:35 +0530

    Change string upper to lowercase and viceversa

---
+++ Strings/ChangeStringCase/ConvertStringCase.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Text;
+
+
+namespace ProblemSolving.Strings.ChangeStringCase
+{
+    public class ConvertStringCase
+    {
+        public void StringCaseConveter(StringBuilder stringToBeConverted)
+        {
+            // ASCII values  of alphabets: A – Z = 65 to 90, a – z = 97 to 122
+
+            for (int i = 0; i < stringToBeConverted.Length; i++)
+            {
+
+                if (stringToBeConverted[i] >= 'a' && stringToBeConverted[i] <= 'z')
+                {
+                    stringToBeConverted[i] = (char) (stringToBeConverted[i] - 32);
+                }
+                else if (stringToBeConverted[i] >= 'A' && stringToBeConverted[i] <= 'Z')
+                {
+                    stringToBeConverted[i] = (char) (stringToBeConverted[i] + 32);
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ Strings/ChangeStringCase/StringCaseConverterUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProblemSolving.Strings.ChangeStringCase
+{
+
+    [TestFixture]
+    public class StringCaseConverterUnitTests
+    {
+        [Test]
+        public void VerifyChangeStringLowerToUpperCase_StringChangedfromLowerToUpper()
+        {
+            StringBuilder input = new StringBuilder("eee");
+            ConvertStringCase stringCase = new ConvertStringCase();
+            stringCase.StringCaseConveter(input);
+            Assert.AreEqual("EEE",input.ToString());
+
+        }
+    }
+}
