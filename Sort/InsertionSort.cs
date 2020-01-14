21690b2eed12bdc389940e84466acaf39312e719
Meenakshi <dgrmeenakshi@gmail.com>
2020-01-14 21:43:15 +0530

    Add Insertion sort

---
+++ Sort/InsertionSort.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ProblemSolving.Sort
+{  
+    public class InsertionSort
+    {
+        public void InsertSortAlgorithm(int[] inputToBeSorted)
+        {
+            for (int i = 1; i < inputToBeSorted.Length; i++)
+            {
+                var temp = inputToBeSorted[i];
+                var j = i - 1;
+                while (j >= 0 && inputToBeSorted[j] > temp)
+                {
+                    inputToBeSorted[j + 1] = inputToBeSorted[j];
+                    j--;
+                }
+
+                inputToBeSorted[j + 1] = temp;
+            }
+        }
+    }
+}
