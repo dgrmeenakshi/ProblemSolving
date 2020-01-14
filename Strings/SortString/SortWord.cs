d11d8ae0a0f56e09f0e103508fca7e4a137219c8
Meenakshi <dgrmeenakshi@gmail.com>
2020-01-14 21:59:22 +0530

    Sort Word

---
+++ Strings/SortString/SortWord.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving.Strings.SortString
+{
+    public class SortWord
+    {
+        public void SortTextWithoutInBuiltFunction(StringBuilder stringInput)
+        {
+            for (int i = 1; i < stringInput.Length; i++)
+            {
+                var temp = stringInput[i];
+                var j = i - 1;
+                while (j >= 0 && stringInput[j] > temp)
+                {
+                    stringInput[j + 1] = stringInput[j];
+                    j--;
+                }
+
+                stringInput[j + 1] = temp;
+
+            }
+
+            Console.WriteLine(stringInput.ToString());
+
+        }
+
+        public void SortTextWithBuiltInFunction(string stringInput)
+        {
+            char[] charArray = stringInput.ToCharArray();
+            Array.Sort(charArray);
+            Console.WriteLine(new string(charArray));
+
+        }
+
+        public void SortTextWithLinq(string stringInput)
+        {
+            Console.WriteLine(stringInput.OrderBy(x => x.ToString()).ToArray());
+
+        }
+    }
+}
